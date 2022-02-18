b829767b5e2d0f8473045bc8d7260aa8f0b80a71
Peilin Li <mauriceli1995@hotmail.com>
2022-02-18 15:43:48 +1100

    setup PostgreSQL databse and created table for clinician and patient

---
+++ clinicMonitor/users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Clinician(models.Model):
+    clinician_name = models.CharField(max_length=200)
+    email_address = models.EmailField(max_length = 254)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '%s, %s, %s' % (self.clinician_name, self.email_address, self.password)
+
+class Patient(models.Model):
+    patient_name = models.CharField(max_length=200)
+    email_address = models.EmailField(max_length = 254)
+    password = models.CharField(max_length=200)
+    clinician = models.ForeignKey(Clinician, on_delete=models.CASCADE)
